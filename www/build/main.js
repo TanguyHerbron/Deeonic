60cc8fea5e4d18e86f4d9fa24b186f989a17291d
Tanguy Herbron <tanguy.herbron@outlook.com>
2019-03-26 10:07:45 +0100

    Merge

--- www/build/main.js
+++ www/build/main.js
@@ -1,5 +1,107 @@
 webpackJsonp([0],{
 
+/***/ 100:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DetailsPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(31);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common_http__ = __webpack_require__(46);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+/**
+ * Generated class for the DetailsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var DetailsPage = (function () {
+    function DetailsPage(navCtrl, navParams, httpClient) {
+        var _this = this;
+        this.navCtrl = navCtrl;
+        this.navParams = navParams;
+        this.httpClient = httpClient;
+        this.isPlaying = null;
+        this.res = navParams.get("item");
+        this.httpClient.get(this.res.album.tracklist)
+            .subscribe(function (data) {
+            _this.tracks = new Array();
+            for (var i = 0; i < data['data'].length; i++) {
+                _this.tracks.push(data['data'][i]);
+            }
+        });
+    }
+    DetailsPage.prototype.playMusic = function () {
+        if (this.isPlaying == null) {
+            this.audio = new Audio();
+            this.audio.src = this.res.preview;
+            this.audio.load();
+            this.audio.play();
+            this.isPlaying = true;
+        }
+        else {
+            if (this.isPlaying) {
+                this.audio.pause();
+                this.isPlaying = false;
+            }
+            else {
+                this.audio.play();
+                this.isPlaying = true;
+            }
+        }
+    };
+    DetailsPage.prototype.time_formater = function (_seconds) {
+        if (typeof _seconds !== "number" || _seconds < 0)
+            return "--";
+        var hours = Math.floor(_seconds / 3600), minutes = Math.floor((_seconds % 3600) / 60), seconds = Math.floor(_seconds % 60);
+        if (this.padTime(hours) != "00") {
+            return this.padTime(hours) + ":" + this.padTime(minutes) + ":" + this.padTime(seconds);
+        }
+        return this.padTime(minutes) + ":" + this.padTime(seconds);
+    };
+    ;
+    DetailsPage.prototype.padTime = function (t) {
+        return t < 10 ? "0" + t : t;
+    };
+    DetailsPage.prototype.cardClicked = function (i) {
+        if (this.isPlaying != null) {
+            this.audio.pause();
+        }
+        this.audio = new Audio();
+        this.audio.src = this.tracks[i].preview;
+        this.audio.load();
+        this.audio.play();
+        this.isPlaying = true;
+    };
+    DetailsPage.prototype.goBack = function () {
+        if (this.audio != undefined)
+            this.audio.pause();
+        this.navCtrl.pop();
+    };
+    DetailsPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-details',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\details\details.html"*/`<!--\n  Generated template for the DetailsPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n\n<ion-content padding>\n\n  <ion-icon name="arrow-back" (click)="goBack()"></ion-icon>\n\n  <div class="player">\n    <div class="player-controls" (click)="playMusic()">\n      <ion-icon *ngIf="!isPlaying" name="play"></ion-icon>\n      <ion-icon *ngIf="isPlaying" name="pause"></ion-icon>\n    </div>\n    <img class="player-cover" [src]="navParams.get(\'item\').album.cover_medium" (click)="playMusic()">\n  </div>\n\n  <ion-list inset *ngFor="let track of tracks; let i = index">\n    <ion-item (click)="cardClicked(i)">\n      <ion-label class="title">{{track.title}}</ion-label>\n      <ion-label class="duration">{{track.duration}}</ion-label>\n    </ion-item>\n  </ion-list>\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\details\details.html"*/,
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */], __WEBPACK_IMPORTED_MODULE_2__angular_common_http__["a" /* HttpClient */]])
+    ], DetailsPage);
+    return DetailsPage;
+}());
+
+//# sourceMappingURL=details.js.map
+
+/***/ }),
+
 /***/ 113:
 /***/ (function(module, exports) {
 
@@ -68,7 +170,7 @@ var TabsPage = (function () {
         this.statusBar.backgroundColorByName("white");
     }
     TabsPage = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/`<ion-tabs>\n  <ion-tab [root]="homeTab" tabTitle="Home" tabIcon="ios-home-outline"></ion-tab>\n  <ion-tab [root]="searchTab" tabTitle="Search" tabIcon="ios-search"></ion-tab>\n  <ion-tab [root]="infoTab" tabTitle="Info" tabIcon="ios-information-circle-outline"></ion-tab>\n</ion-tabs>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/`<ion-tabs>\n  <ion-tab [root]="homeTab" tabTitle="Home" tabIcon="ios-home-outline"></ion-tab>\n  <ion-tab [root]="searchTab" tabTitle="Search" tabIcon="ios-search"></ion-tab>\n  <ion-tab [root]="tab3Root" tabTitle="Info" tabIcon="ios-information-circle-outline"></ion-tab>\n</ion-tabs>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_3__ionic_native_status_bar__["a" /* StatusBar */]])
     ], TabsPage);
@@ -142,7 +244,7 @@ var SearchPage = (function () {
     };
     SearchPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-search',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\search\search.html"*/`<ion-content padding>\n\n  <div class="top-spacing"></div>\n\n  <ion-searchbar (ionInput)="onInput($event)"></ion-searchbar>\n\n  <div *ngIf="!showResult">\n    <ion-list inset *ngFor="let radio of radios; let i = index">\n      <ion-item *ngIf="i % 2 == 0" class="radio-item">\n        <ion-card class="radio-card" (click)="radioClicked(i)">\n          <img [src]="radios[i].picture_medium" alt="cover">\n          <ion-card-content>{{radios[i].title}}</ion-card-content>\n        </ion-card>\n        <ion-card class="radio-card second-card" (click)="radioClicked(i)">\n          <img [src]="radios[i+1].picture_medium" alt="cover">\n          <ion-card-content>{{radios[i+1].title}}</ion-card-content>\n        </ion-card>\n      </ion-item>\n    </ion-list>\n  </div>\n\n  <div *ngIf="showResult">\n    <ion-list inset *ngFor="let data of searchResults; let i = index">\n      <ion-item>\n        <ion-card class="card-album" (click)="albumClicked(i)">\n          <img class="album-cover" [src]="data.album.cover_medium" alt="cover">\n          <div class="card-text">\n            <ion-card-header>\n              {{data.title}}\n            </ion-card-header>\n            <p>{{data.artist.name}}</p>\n            <ion-card-content>{{data.album.title}}</ion-card-content>\n          </div>\n        </ion-card>\n      </ion-item>\n    </ion-list>\n  </div>\n\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\search\search.html"*/
+            selector: 'page-search',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\search\search.html"*/`<ion-content padding>\n  <ion-searchbar (ionInput)="onInput($event)"></ion-searchbar>\n  <ion-list inset *ngFor="let data of searchResults; let i = index">\n    <ion-item>\n      <ion-card (click)="cardClicked(i)">\n        <img [src]="data.album.cover_medium" alt="cover">\n        <div class="card-text">\n          <ion-card-header>\n            {{data.title}}\n          </ion-card-header>\n          <p>{{data.artist.name}}</p>\n          <ion-card-content>{{data.album.title}}</ion-card-content>\n        </div>\n      </ion-card>\n    </ion-item>\n  </ion-list>\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\search\search.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__angular_common_http__["a" /* HttpClient */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["b" /* App */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["c" /* Events */]])
     ], SearchPage);
@@ -331,7 +433,7 @@ var HomePage = (function () {
     };
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\home\home.html"*/`<!--\n  Generated template for the HomePage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n\n<ion-content>\n\n  <div class="top-spacing"></div>\n\n  <ion-badge class="trending-badge" color="primary">Best playlists</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX class="scroll-playlist">\n      <div class="scroll-item">\n        <ion-col *ngFor="let playlist of trendingPlaylists; let i = index">\n          <div class="card-playlist card-1" (click)="playlistclicked(i)">\n            <img [src]="playlist.picture_medium">\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n  <ion-badge class="trending-badge" color="primary">Trending albums</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX>\n      <div class="scroll-item">\n        <ion-col *ngFor="let album of trendingAlbums; let i = index">\n          <div class="card-trending-album card-1" (click)="albumClicked(i)">\n            <img [src]="album.cover_medium">\n            <ion-label>{{album.title}}</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n  <ion-badge class="trending-badge" color="primary">Top artists</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX class="scroll-artist">\n      <div class="scroll-item">\n        <ion-col *ngFor="let artist of trendingArtists; let i = index">\n          <div class="card-list" (click)="artistClicked(i)">\n            <div class="card-artist card-1">\n              <img [src]="artist.picture_medium">\n            </div>\n            <ion-label>{{artist.name}}</ion-label>\n            <ion-label class="fan-label">{{artist.nb_fan_form}} fans</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\home\home.html"*/,
+            selector: 'page-home',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\home\home.html"*/`<!--\n  Generated template for the HomePage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n\n<ion-content>\n\n  <ion-badge class="trending-badge" color="primary">Trending albums</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX>\n      <div class="scroll-item">\n        <ion-col *ngFor="let album of trendingAlbums; let i = index">\n          <div class="card-album card-1" (click)="albumClicked(i)">\n            <img [src]="album.cover_medium">\n            <ion-label>{{album.title}}</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n  <ion-badge class="trending-badge" color="primary">Top artists</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX>\n      <div class="scroll-item scroll-artist">\n        <ion-col *ngFor="let artist of trendingArtists; let i = index">\n          <div class="card-list" (click)="artistClicked(i)">\n            <div class="card-artist card-1">\n              <img [src]="artist.picture_medium">\n            </div>\n            <ion-label>{{artist.name}}</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\home\home.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__angular_common_http__["a" /* HttpClient */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["c" /* Events */]])
     ], HomePage);
@@ -570,7 +672,7 @@ var MyApp = (function () {
         });
     };
     MyApp = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"E:\Github\Deeonic\src\app\app.html"*/`<ion-nav [root]="rootPage"></ion-nav>\n`/*ion-inline-end:"E:\Github\Deeonic\src\app\app.html"*/,
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"C:\Users\Guitoune\Documents\GitKraken\Deeonic\src\app\app.html"*/`<ion-nav [root]="rootPage"></ion-nav>\n`/*ion-inline-end:"C:\Users\Guitoune\Documents\GitKraken\Deeonic\src\app\app.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["a" /* AlertController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["c" /* Events */]])
     ], MyApp);
@@ -583,3 +685,6 @@ var MyApp = (function () {
 
 },[203]);
 //# sourceMappingURL=main.js.map
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/`<ion-tabs>\n  <ion-tab [root]="homeTab" tabTitle="Home" tabIcon="ios-home-outline"></ion-tab>\n  <ion-tab [root]="searchTab" tabTitle="Search" tabIcon="ios-search"></ion-tab>\n  <ion-tab [root]="infoTab" tabTitle="Info" tabIcon="ios-information-circle-outline"></ion-tab>\n</ion-tabs>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\tabs\tabs.html"*/
+            selector: 'page-search',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\search\search.html"*/`<ion-content padding>\n\n  <div class="top-spacing"></div>\n\n  <ion-searchbar (ionInput)="onInput($event)"></ion-searchbar>\n\n  <div *ngIf="!showResult">\n    <ion-list inset *ngFor="let radio of radios; let i = index">\n      <ion-item *ngIf="i % 2 == 0" class="radio-item">\n        <ion-card class="radio-card" (click)="radioClicked(i)">\n          <img [src]="radios[i].picture_medium" alt="cover">\n          <ion-card-content>{{radios[i].title}}</ion-card-content>\n        </ion-card>\n        <ion-card class="radio-card second-card" (click)="radioClicked(i)">\n          <img [src]="radios[i+1].picture_medium" alt="cover">\n          <ion-card-content>{{radios[i+1].title}}</ion-card-content>\n        </ion-card>\n      </ion-item>\n    </ion-list>\n  </div>\n\n  <div *ngIf="showResult">\n    <ion-list inset *ngFor="let data of searchResults; let i = index">\n      <ion-item>\n        <ion-card class="card-album" (click)="albumClicked(i)">\n          <img class="album-cover" [src]="data.album.cover_medium" alt="cover">\n          <div class="card-text">\n            <ion-card-header>\n              {{data.title}}\n            </ion-card-header>\n            <p>{{data.artist.name}}</p>\n            <ion-card-content>{{data.album.title}}</ion-card-content>\n          </div>\n        </ion-card>\n      </ion-item>\n    </ion-list>\n  </div>\n\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\search\search.html"*/
+            selector: 'page-home',template:/*ion-inline-start:"E:\Github\Deeonic\src\pages\home\home.html"*/`<!--\n  Generated template for the HomePage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n\n<ion-content>\n\n  <div class="top-spacing"></div>\n\n  <ion-badge class="trending-badge" color="primary">Best playlists</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX class="scroll-playlist">\n      <div class="scroll-item">\n        <ion-col *ngFor="let playlist of trendingPlaylists; let i = index">\n          <div class="card-playlist card-1" (click)="playlistclicked(i)">\n            <img [src]="playlist.picture_medium">\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n  <ion-badge class="trending-badge" color="primary">Trending albums</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX>\n      <div class="scroll-item">\n        <ion-col *ngFor="let album of trendingAlbums; let i = index">\n          <div class="card-trending-album card-1" (click)="albumClicked(i)">\n            <img [src]="album.cover_medium">\n            <ion-label>{{album.title}}</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n  <ion-badge class="trending-badge" color="primary">Top artists</ion-badge>\n\n  <ion-item>\n    <ion-scroll scrollX class="scroll-artist">\n      <div class="scroll-item">\n        <ion-col *ngFor="let artist of trendingArtists; let i = index">\n          <div class="card-list" (click)="artistClicked(i)">\n            <div class="card-artist card-1">\n              <img [src]="artist.picture_medium">\n            </div>\n            <ion-label>{{artist.name}}</ion-label>\n            <ion-label class="fan-label">{{artist.nb_fan_form}} fans</ion-label>\n          </div>\n        </ion-col>\n      </div>\n    </ion-scroll>\n  </ion-item>\n\n</ion-content>\n`/*ion-inline-end:"E:\Github\Deeonic\src\pages\home\home.html"*/,
